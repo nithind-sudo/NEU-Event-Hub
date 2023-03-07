1d2a5387ac47e6c52492c9a847cff10e2c02edd3
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-07 01:50:40 -0500

    Added code to create connection to mongodb

--- server/mongodb/db.js
+++ server/mongodb/db.js
@@ -1,35 +1,23 @@
-const { Sequelize } = require('sequelize');
+const mongoose = require('mongoose');
 const {
     dbName,
     hostUrl,
     userName,
     password,
     nodeEnv,
-    dbPoolConfig,
   } = require("./config");
   
 // Sample URI : mongodb://myuser:mypassword@localhost:27017/mydb
 
 // const uri = 'mongodb://localhost:27017/mydb';
+
 const uri = `mongodb://${userName}:${password}@${hostUrl}:27017/${dbName}`
 
-const sequelize = new Sequelize(uri, {
-  dialect: 'mongodb',
-  dialectOptions: {
-    ssl: false,
-  },
-});
+console.log("URI of mongodb to connect : ", uri);
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
+mongoose.connect(uri);
+const db = mongoose.connection;
+db.on('error', (error) => {console.log(error)});
+db.once('open', () => {console.log("Connected to DB")});
 
-module.exports = {
-    sequelize,
-    DataTypes,
-};
-  
+module.exports = db;
