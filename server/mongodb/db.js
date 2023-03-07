d39d52d199733f1cafafcc0f321349a736ebada6
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-07 00:48:42 -0500

    Added code to print if connection is successful to db.js

--- server/mongodb/db.js
+++ server/mongodb/db.js
@@ -19,3 +19,17 @@ const sequelize = new Sequelize(uri, {
     ssl: false,
   },
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+module.exports = {
+    sequelize,
+    DataTypes,
+};
+  
