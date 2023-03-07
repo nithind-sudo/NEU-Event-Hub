e1af87ee8ed5fbf80e70e25cc4e81cb01eb13d33
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-07 18:35:50 -0500

    Added POST endpoint to create row in DB

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -1,8 +1,9 @@
 const User = require("../models/users");
-
 class UserService{
 
     async createUser(userPayload){
+        console.log("Payload being used to create : ", userPayload);
+        // const user = new User(userPayload);
         return await User.create(userPayload);
     }
 
