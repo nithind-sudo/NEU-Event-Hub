316c7d490405cafb86cfeeaae1782d3670121570
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-09 01:47:12 -0500

    Added changes in the code to increment user-id upon POST

--- server/models/users.js
+++ server/models/users.js
@@ -1,9 +1,22 @@
 const mongoose = require("../mongodb/db");
+const userCounter = require("./userCounter");
 const { Schema, model } = mongoose;
-// const AutoIncrement = require('mongoose-sequence')(mongoose);
 
+
+async function getNextSequenceValue(sequenceName) {
+  const counter = await userCounter.findOneAndUpdate(
+    { _id: sequenceName },
+    { $inc: { seq: 1 } },
+    { new: true, upsert: true }
+  );
+  return counter.seq;
+}
 
 const userSchema = new Schema({
+  user_id: { 
+    type: Number, 
+    unique: true 
+  },
   username : {
     type : String,
     required : true,
@@ -32,8 +45,20 @@ const userSchema = new Schema({
   },
 });
 
-// userSchema.plugin(AutoIncrement, { inc_field: 'userId' });
 
-// generates a collection called users in DB
+userSchema.pre('save', async function(next) {
+  console.log("***** Inside pre middleware mongodb *****")
+  if (!this.isNew) {
+    return next();
+  }
+
+  const doc = this;
+  const seq = await getNextSequenceValue('user_id');
+
+  doc.user_id = seq;
+
+  return next();
+});
+
 const User = model("User", userSchema);
 module.exports = User;
