d05af881ad83fd572ed80a70b90e9174c5ea6eda
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-09 02:26:07 -0500

    Added changes in the code to PATCH end point

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -24,4 +24,60 @@ exports.createUser = async (req, res) => {
     }
 };
 
+exports.getUserInfo = async (req, res) => {
+    const {user_id} = req.params;
+    try{
+        const userInfo = await userService.getUser(user_id);
+        res.status(200).send(userInfo);    
+    }catch(e){
+        res.status(400).send({"message" : "400 Bad Request", error : error.message});
+    }
+}
 
+exports.deleteUserInfo = async (req, res) => {
+    const {user_id} = req.params;
+    console.log("User -id input : ", user_id);
+    try{
+        const deletedCnt = await userService.deleteUser(user_id);
+        // Sample successful o/p : { acknowledged: true, deletedCount: 1 }
+        console.log("Deleted Row count : ", deletedCnt);
+        if(deletedCnt.deletedCount === 1){
+            res.status(204).send({});
+        }else{
+            res.status(400).send({message : "400 Bad Request"});
+        }
+
+    }catch(e){
+        res.status(500).send({"message" : "500 Internal Server Error", message : e.message});
+    }
+}
+
+exports.patchUserInfo = async (req, res) => {
+    const {first_name, last_name, password, username} = req.body;
+    const patchInfo = { 
+        ...(first_name && { first_name }), 
+        ...(last_name && { last_name }),
+        ...(username && { username}),
+        // account_updated : new Date()
+    };
+    if(!password){
+        // password field is not present
+        try{
+            const updatedInfo = await userService.patchUser(patchInfo);
+            res.status(201).send(updatedInfo);
+        }catch(e){
+            res.status(400).send({message : "400 Bad Request", error : e.message});
+        }
+        
+    }else{
+        // password field is present
+        try{
+            const hash = await authUtils.generateHash(password);
+            const updatedPatchPayload = {password : hash, ...patchInfo};
+            const updatedInfo = await userService.patchUser(patchInfo);
+            res.status(201).send(updatedInfo);    
+        }catch(e){
+            res.status(400).send({message : "400 Bad Request", error : e.message});
+        }  
+    }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,8 +5,13 @@ const userController = require("../controllers/users");
 
 const loggerMiddleware = require("../middleware/loggerMiddleware");
 
+router.get("/:user_id", [loggerMiddleware], userController.getUserInfo);
+
 router.post("/", [loggerMiddleware], userController.createUser);
 
+router.delete("/:user_id", [loggerMiddleware], userController.deleteUserInfo);
+
+router.patch("/:user_id", [loggerMiddleware], userController.patchUserInfo);
 
 module.exports = router;
 
