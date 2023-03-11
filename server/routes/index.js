18dc88759463e682bfb4b336f3396c2c2f068e80
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-11 08:14:28 -0500

    Added base code for adding sessions to the project

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,10 @@ const index_controller = require("../controllers/index");
 
 router.get("/", index_controller.welcome);
 
+router.get("/login", index_controller.login);
+
+router.get("/login", index_controller.logout);
+
 router.get("/healthz", index_controller.healthz);
 
 module.exports = router;
