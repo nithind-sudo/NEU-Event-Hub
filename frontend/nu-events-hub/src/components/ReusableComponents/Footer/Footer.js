b570906eb653a9f2bb2cdab8593cb0502117f12f
sstej88 <saitejsunkara9@gmail.com>
2023-03-22 12:45:40 -0400

    Footer updated

--- frontend/nu-events-hub/src/components/ReusableComponents/Footer/Footer.js
+++ frontend/nu-events-hub/src/components/ReusableComponents/Footer/Footer.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../../styles/ReusableStyles/Footer/footer.css'
 
 const Footer = ()=>{
     return (
-        <div class="container border-top">
-            
-            <footer class="py-3 my-4">
-            <b><p class="text-center text-muted">Developers</p></b>
-            
-            <ul class="nav justify-content-center pb-3 mb-3">
-                <li class="nav-item"><a href="https://github.com/nithin-bharadwaj-369-NEU" target="_blank" class="nav-link px-2 text-muted">Nithin Bhardwaj</a></li>
-                <li class="nav-item"><a href="https://github.com/kaustubh9702" target="_blank" class="nav-link px-2 text-muted">Kaustubh</a></li>
-                <li class="nav-item"><a href="https://github.com/akhileshkavitkar99" target="_blank" class="nav-link px-2 text-muted">Akhilesh Kavitkar</a></li>
-                <li class="nav-item"><a href="https://github.com/sstej88" target="_blank" class="nav-link px-2 text-muted">Sai Tej Sunkara</a></li>
-            </ul>
+        <footer className='container-fluid applyNUTheme border-top'>
+            <div className='container'>
+                <div className='py-3 my-4'>
+                <b><p className='text-center text-muted'>Developers</p></b>
+                
+                <ul className='nav justify-content-center pb-3 mb-3'>
+                    <li className='nav-item'><a href='https://github.com/nithin-bharadwaj-369-NEU' target='_blank' className='nav-link px-2 nuLink'>Nithin Bhardwaj</a></li>
+                    <li className='nav-item'><a href='https://github.com/kaustubh9702' target='_blank' className='nav-link px-2 nuLink'>Kaustubh</a></li>
+                    <li className='nav-item'><a href='https://github.com/akhileshkavitkar99' target='_blank' className='nav-link px-2 nuLink'>Akhilesh Kavitkar</a></li>
+                    <li className='nav-item'><a href='https://github.com/sstej88' target='_blank' className='nav-link px-2 nuLink'>Sai Tej Sunkara</a></li>
+                </ul>
 
-            <b><p class="text-center text-muted">&copy; 2023 NU-Events Hub, Educational Webpage</p></b>
-            </footer>
-        </div>
+                <b><p className='text-center'>&copy; 2023 NU-Events Hub, Educational Webpage</p></b>
+                </div>
+            </div>
+        </footer>
     );
 };
 
