a5d19e8ea0db7f62e80f1a5d3d0474ed19dacf3d
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-02 13:59:16 -0400

    Added changes to the Login, Landing Page componetns

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+import Login from "./pages/Login/Login";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import { useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return <div className="App"></div>;
 }
 
 export default App;
