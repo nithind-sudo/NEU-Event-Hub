54b75a18e66fdaedca5f03416e42a586a243179c
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-02 17:50:59 -0400

    Added changes to hit the backend API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 import Login from "./pages/Login/Login";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return <div className="App">
-    {/* { isLoggedIn && <Login />} */}
-    <Login />
-  </div>;
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <BrowserRouter>
+      {isLoggedIn && <LandingPage />}
+      <Routes>
+        <Route path="/" element={<Login handleLogin={handleLogin} />}></Route>
+        {isLoggedIn && <>
+          Need to Add paths here to multiple pages for navigation
+        </>}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -7,8 +7,42 @@ import Form from "react-bootstrap/Form";
 import { Container } from "react-bootstrap";
 import "./Login.css";
 import loginImage from "../../assets/login_image.jpg";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import MyToast from "../../components/ui/Toast";
 
-export default function Login() {
+
+const LOGIN_URL = "http://localhost:3000/v1/session";
+
+export default function Login({ ...props }) {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(LOGIN_URL, { email, password });
+      console.log(` *** Response from Login End Point : ${response}`);
+      if (response.data.success) {
+        props.handleLogin();
+        navigate("/main");
+      } else {
+        setError("Invalid Email or Address");
+        setShowAlert(true);
+      }
+    } catch (e) {
+      console.log("**** Error while logging in:", e);
+      setError("Invalid Email or Address");
+      setShowAlert(true);
+    }
+  };
+
   return (
     <div className="login-page">
       <div className="flex-column">
@@ -21,24 +55,45 @@ export default function Login() {
             <Form.Group controlId="userEmail">
               <CustomLabel>
                 Username
-                <TextInput type="text" className="login-input" />
+                <TextInput
+                  type="text"
+                  value={email}
+                  className="login-input"
+                  placeholder={"Email..."}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </CustomLabel>
             </Form.Group>
             <Form.Group controlId="userPassword">
               <CustomLabel>
                 Password
-                <TextInput type="password" className="login-input" />
+                <TextInput
+                  type="password"
+                  placeholder={"Password..."}
+                  value={password}
+                  className="login-input"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </CustomLabel>
             </Form.Group>
             <Button
               variant="danger"
               text={"Sign In"}
+              onClick={handleSignIn}
               className="login-button"
             ></Button>
             <CustomLabel>Don't have an account? Create account</CustomLabel>
           </div>
         </Container>
       </Form>
+      {showAlert && (
+        <MyToast
+          bg={"danger"}
+          show={showAlert}
+          onClose={() => setShowAlert(false)}
+          message={error}
+        />
+      )}
     </div>
   );
 }
