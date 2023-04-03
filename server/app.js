12763c3f545e2748d19cd80b99732817a33dfc1d
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-03 12:29:57 -0400

    Updated the result of session api after successful call

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 
 const {mongoose, mongooseConnection} = require('./mongodb/db');
 
 const app = express()
 
+app.use(cookieParser());
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(
