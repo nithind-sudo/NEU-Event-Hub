876969ee69dd50f23582f57c4be1625399869593
sstej88 <saitejsunkara9@gmail.com>
2023-04-06 00:52:36 -0400

    Categories with Links applied

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const events = require('./eventCategories');
+const links = require('./eventLinks');
 const router = express.Router()
 
 const getCategories = (req, res, err)=>{
     const categoriesJson = {
-        categories: events
+        categories: events,
+        links: links
     };
     res.json(categoriesJson);
 }
