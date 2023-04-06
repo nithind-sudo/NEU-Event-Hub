85b3737f9ec3617a82213e043dc2e5ac2b08ffe1
sstej88 <saitejsunkara9@gmail.com>
2023-04-06 13:12:38 -0400

    Categories updated

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./styles/Category.css";
 import { useParams } from "react-router-dom";
+import studentOrganized from "../../assets/studentOrganized.jpeg";
 
 const Category = () => {
   let { categoryName } = useParams();
@@ -20,8 +21,17 @@ const Category = () => {
   return (
     <React.Fragment>
       <div className="container">
-        <div className="rounded setJumbotronHeight text-center p-5 my-5">
-          <div className="display-5">{category.name}</div>
+        <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                <div className="rounded setJumbotronHeight text-center p-5 my-5">
+                    <div className="display-6">{category.name}</div>
+                </div>
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div className="rounded my-5">
+                    <img src={studentOrganized} alt="Students Organized Image" className="img-thumbnail" />
+                </div>
+            </div>
         </div>
       </div>
     </React.Fragment>
