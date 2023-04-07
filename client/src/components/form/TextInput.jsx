bce41f4033f98c7e1450894da4d2dddf4473b04a
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-07 13:17:47 -0400

    Added favicon to the website and changed the title of the website

--- client/src/components/form/TextInput.jsx
+++ client/src/components/form/TextInput.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { FormControl } from "react-bootstrap";
 
-const TextInput = ({ text, value, onChange, placeholder, ...props }) => {
+const TextInput = ({ text, value, onChange, className, placeholder, ...props }) => {
   return (
     <FormControl
       type={text}
       value={value}
       onChange={onChange}
+      className={className}
       placeholder={placeholder}
       {...props}
     />
