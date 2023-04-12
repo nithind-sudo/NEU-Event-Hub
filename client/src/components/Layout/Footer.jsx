022404088535be74a501889936cceccad847c894
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-12 11:51:58 -0400

    Updated Landing Page code to Add the Courasel, header and footer components

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -1,16 +1,18 @@
 import Container from "react-bootstrap/Container";
 
-export default function Footer(){
-    return <>
-        <footer className="bg-light py-3 fixed-bottom">
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-dark py-3 fixed-bottom">
         <Container>
           <div className="text-center">
-            <p className="mb-0">
-              &copy; {new Date().getFullYear()} Made by Nithin Bharadwaj. All rights
-              reserved.
+            <p className="mb-0 text-light">
+              &copy; {new Date().getFullYear()} Made by Web Design Team. All
+              rights reserved.
             </p>
           </div>
         </Container>
       </footer>
     </>
+  );
 }
