c2def56d47645398be142899c3bab12262ada6bc
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-12 00:11:18 -0400

    Updated backend code to create, delete and get event

--- server/services/EventService.js
+++ server/services/EventService.js
@@ -1,17 +1,32 @@
+const UserService = require("../services/UserService");
 const Event = require("../models/events");
 
 class EventService {
-  async createUser(eventPayload) {
-    const event = new Event(eventPayload);
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  async createEvent(eventPayload) {
+    const { organizer, ...restPayload } = eventPayload;
+    const user = await this.userService.getUserById(organizer);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    
+    const event = new Event({
+      ...restPayload,
+      organizer : user[0]._id,
+    });
+
     return await event.save();
   }
 
-  async getEvent(event_id) {
-    return await Event.find({ event_id: event_id }).exec();
+  async getEventInfo(event_id) {
+    return await Event.find({ event_id }).exec();
   }
 
   async deleteEvent(event_id) {
-    return await Event.deleteOne({ event_id: event_id }).exec();
+    return await Event.deleteOne({ event_id }).exec();
   }
 
   async patchEvent(event_id, payload) {
@@ -19,3 +34,5 @@ class EventService {
     return await Event.findOneAndUpdate({ event_id }, { ...payload });
   }
 }
+
+module.exports = EventService;
