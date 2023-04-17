3e02527bf525b779a1a6d1bf6ff57b94356c1b20
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-16 20:20:20 -0400

    Updated Session endpoints

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -1,15 +1,9 @@
-function withoutProperty(obj, property) {
-  const { [property]: unused, ...rest } = obj;
-
-  return rest;
-}
-
 exports.getSessionInfo = async (req, res) => {
   if (req.session) {
+    const sessionData = req.session.sessionData;
     res.json({
-      status: "success",
-      message: "Session information retrieved",
-      data: req.session,
+      success: true,
+      sessionData,
     });
   } else {
     res.status(404).json({
@@ -22,22 +16,23 @@ exports.getSessionInfo = async (req, res) => {
 exports.generateSession = async (req, res) => {
   console.log("Inside generate Session of Session Controller");
   const userInfo = req.userInfo;
-  const userDataToSend = {
+  req.session.authorized = true;
+  const sessionData = {
     username: userInfo.username,
     role: userInfo.role,
     user_id: userInfo.user_id,
     events_booked: userInfo.events_booked,
     favorites: userInfo.favorites,
     listings: userInfo.listings,
-    isActivated : userInfo.isActivated,
-    isVerified : userInfo.isVerified
+    isActivated: userInfo.isActivated,
+    isVerified: userInfo.isVerified,
   };
+  req.session.sessionData = sessionData;
 
-  req.session.authorized = true;
   res.status(201).send({
     message: `Session Created for User: ${req.username}`,
     success: true,
-    user: userDataToSend,
+    sessionData,
   });
 };
 
