f96acfb00abd1b819e40a45baef90e62edca2754
akhileshkavitkar99 <kavitkar.ak@northeastern.edu>
2023-04-18 20:52:05 -0400

    Update footer.jsx

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -3,20 +3,28 @@ import "../../styles/Footer/Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div className="footer-content">
-        <p>&copy; 2023 Northeastern Events</p>
-        <div className="about">
-        <h4>Contact Us</h4>
-            <p>
-              Northeastern University
-              <br />
-              360 Huntington Ave, Boston, MA 02115
-              <br />
-              Email: <a href="mailto:events@northeastern.edu">events@northeastern.edu</a>
-            </p>
+    <div className="bg-dark text-white py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <p>&copy; 2023 Northeastern Events</p>
+          </div>
+          <div className="col-lg-6">
+            <div className="about">
+              <h4>Contact Us</h4>
+              <p>
+                Northeastern University
+                <br />
+                360 Huntington Ave, Boston, MA 02115
+                <br />
+                Email:{" "}
+                <a href="mailto:events@northeastern.edu" className="text-white">
+                  events@northeastern.edu
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-        
       </div>
     </div>
   );
