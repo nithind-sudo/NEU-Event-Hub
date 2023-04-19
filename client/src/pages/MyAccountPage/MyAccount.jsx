0a2f362ce0f451ed5a34e223d7e1e49f622c8df5
sstej88 <saitejsunkara9@gmail.com>
2023-04-19 17:04:11 -0400

    Fixed bug

--- client/src/pages/MyAccountPage/MyAccount.jsx
+++ client/src/pages/MyAccountPage/MyAccount.jsx
@@ -89,7 +89,7 @@ export default function MyAccount({ handlelogout }) {
       <div className="myAccount-page">
         <Container fluid>
           <div className="container">
-            <div className="card" style="width: 18rem;">
+            <div className="card">
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
