b0362aecdf1003085283a9b43481add0dc051870
sstej88 <saitejsunkara9@gmail.com>
2023-04-20 00:55:51 -0400

    banner added

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -6,11 +6,23 @@ import MyCarousel from "../../components/Layout/MyCarousel";
 import carouselData from "./CarouselData.json";
 import CategoryView from "../../components/CategoryView/CategoryView";
 import AllEvents from "../AllEvents/AllEvents";
+import bannerImage from "../../assets-pack/home/banner.jpeg";
+import "./LandingPage.css";
 
 export default function LandingPage(props) {
   return (
     <div className="pb-5">
-    <Navbar handlelogout={props.handlelogout} />
+      <div className="container">
+        <div className="row">
+          <img
+            src={bannerImage}
+            alt="Banner Image"
+            className="bannerImageStyle mt-5 pt-4"
+          />
+        </div>
+      </div>
+
+      <Navbar handlelogout={props.handlelogout} />
       <Container fluid>
         <CategoryView />
         <MyCarousel className="" carouselData={carouselData} />
