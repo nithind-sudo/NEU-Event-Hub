cfbe71feca038bdf2a807d7e0cd4605bcb9caf07
kaustubh9702 <kaustubhlawale4@gmail.com>
2023-04-21 19:09:36 -0400

    Final changes!!

--- client/src/pages/AboutMe/AboutMe.jsx
+++ client/src/pages/AboutMe/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './AboutMePage.css';
+import './AboutMe.css';
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/footer";
 
