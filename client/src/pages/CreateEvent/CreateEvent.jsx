918b658cf75076787752a9a21e9da53ec4482600
sstej88 <saitejsunkara9@gmail.com>
2023-04-21 02:52:44 -0400

    Create Event Styled

--- client/src/pages/CreateEvent/CreateEvent.jsx
+++ client/src/pages/CreateEvent/CreateEvent.jsx
@@ -14,13 +14,14 @@ import Button from "../../components/ui/Button";
 import { fetchCreateEvent } from "../../apiClient";
 import { EventManagementState } from "../../contexts/context";
 import MyToast from "../../components/ui/Toast";
+import TextArea from "../../components/form/TextArea";
 
 export default function CreateEvent({
   handlelogout,
   showAlert,
   setShowAlert,
   setError,
-  error
+  error,
 }) {
   const schema = Joi.object({
     title: Joi.string().min(2).required(),
@@ -137,211 +138,196 @@ export default function CreateEvent({
   return (
     <div>
       <Navbar handlelogout={handlelogout} />
-      <Form>
-        <Container>
-          <div className="create-event-container">
-            <Row>
-              <h1>Basic Info</h1>
-              <h6>
-                Name your event and tell event-goers why they should come. Add
-                details that highlight what makes it unique.
-              </h6>
-            </Row>
-            <Row className="justify-content-center">
-              <Col>
-                <Form.Group controlId="eventTitle">
-                  <CustomLabel>
-                    <label className="lead mt-3 mb-1">Event Title</label>
-                    <TextInput
-                      type="text"
-                      value={formData.title}
-                      className=""
-                      onChange={(e) =>
-                        handleFieldChange("title", e.target.value)
-                      }
-                      onBlur={() => handleFieldBlur("title")}
-                      isInvalid={!!errorValidation.title}
-                      placeholder={"Be Clear and Descriptive"}
-                    />
-                  </CustomLabel>
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col>
-                <Form.Group controlId="eventDescription">
-                  <CustomLabel>
-                    <label className="lead mt-3 mb-1">Event Description</label>
-                    <TextInput
-                      type="text"
-                      value={formData.description}
-                      className=""
-                      onChange={(e) =>
-                        handleFieldChange("description", e.target.value)
-                      }
-                      onBlur={() => handleFieldBlur("description")}
-                      isInvalid={!!errorValidation.description}
-                      placeholder={"Tell attendees who is organizing the event"}
-                    />
-                  </CustomLabel>
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col>
-                <Form.Group controlId="eventTag">
-                  <CustomLabel>
-                    <label className="lead mt-3 mb-1">Event Tag</label>
-                    <Dropdown
-                      className="custom-dropdown custom-dropdown-width"
-                      onSelect={handleSelect}
-                    >
-                      <Dropdown.Toggle
-                        variant="outline-secondary"
-                        id="dropdown-basic"
-                      >
-                        {selectedTag}
-                      </Dropdown.Toggle>
-
-                      <Dropdown.Menu>
-                        <Dropdown.Item eventKey="Students Organized Events">
-                        Students Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="Professors Organized Events">
-                        Professors Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="Speakers Organized Events">
-                        Speakers Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="Northeastern's Management Events">
-                        Northeastern's Management Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="Khoury College of Computer Science Organized Events">
-                        Khoury College of Computer Science Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="College of Engineering Organized Events">
-                        College of Engineering Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="College of Professional Studies Organized Events">
-                        College of Professional Studies Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="College of Science Organized Events">
-                        College of Science Organized Events
-                        </Dropdown.Item>
-                        <Dropdown.Item eventKey="D'Amore College of Management Events">
-                        D'Amore College of Management Events
-                        </Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </CustomLabel>
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <h1>Location</h1>
-              <h6>
-                Help people in the area discover your event and let attendees
-                know where to show up.
-              </h6>
-            </Row>
-            <Row>
-              <LocationInput onLocationChange={handleLocationChange} />
-            </Row>
-            <br />
-            <Row>
-              <h1>Date and time</h1>
-              <h6>
-                Tell event-goers when your event starts and ends so they can
-                make plans to attend.
-              </h6>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group controlId="eventStartTime">
-                  <CustomLabel>
-                    <label className="lead mt-3 mb-1">
-                      Pick Start Date and Time
-                    </label>
-                    
-                    <div className="">
-                      <DatePicker
-                        defaultValue={new Date()}
-                        value={startDate}
-                        onChange={handleStartDateChange}
-                        className="w-3/5"
-                      />
-                      <br />
-                      <TimeInput
-                        defaultValue={new Date()}
-                        value={startTime}
-                        onChange={handleStartTimeChange}
-                        className="w-2/5 mt-0"
-                        use12HourClock
-                      />
+      <div className="py-5 setBackgroundForCreateEvent">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-2 col-lg-3 col-xl-3"></div>
+            <div className="col-xs-12 col-sm-12 col-md-8 col-lg-6 col-xl-6">
+              <div class="card text-dark">
+                <div class="card-body">
+                  <div className="container">
+                    <div class="card-title display-6">Create Event</div>
+                    <div class="card-title h6">
+                      You're an Admin. You can create an Event!
                     </div>
-                  </CustomLabel>
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Group controlId="eventEndTime">
-                  <CustomLabel>
-                    <label className="lead mt-3 mb-1">
-                      Pick End Date and Time
-                    </label>
-                    <div className="">
-                      <DatePicker
-                        defaultValue={new Date()}
-                        value={endDate}
-                        onChange={handleEndDateChange}
-                        className="w-3/5"
-                      />
-                      <br />
-                      <TimeInput
-                        defaultValue={new Date()}
-                        value={endTime}
-                        onChange={handleEndTimeChange}
-                        className="w-2/5 mt-0"
-                        use12HourClock
-                      />
+                  </div>
+                  <div class="card-subtitle mb-2 text-body-secondary">
+                    <div className="container">
+                      <div className="row">
+                        <label className="lead mt-3 mb-1">Event Title</label>
+                        <div className="container">
+                          <TextInput
+                            type="text"
+                            value={formData.title}
+                            className=""
+                            onChange={(e) =>
+                              handleFieldChange("title", e.target.value)
+                            }
+                            onBlur={() => handleFieldBlur("title")}
+                            isInvalid={!!errorValidation.title}
+                            placeholder={"Enter your Event Title"}
+                          />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <label className="lead mt-3 mb-1">
+                          Event Description
+                        </label>
+                        <div className="container">
+                          <TextArea
+                            type="text"
+                            value={formData.description}
+                            className=""
+                            onChange={(e) =>
+                              handleFieldChange("description", e.target.value)
+                            }
+                            onBlur={() => handleFieldBlur("description")}
+                            isInvalid={!!errorValidation.description}
+                            placeholder={
+                              "Tell attendees who is organizing the event"
+                            }
+                          />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <label className="lead mt-3 mb-1">
+                          Who is Organizing this Event?
+                        </label>
+                        <Dropdown
+                          className=""
+                          onSelect={handleSelect}>
+                          <Dropdown.Toggle
+                            variant="outline-secondary"
+                            id="dropdown-basic" style={{width: '100%'}}>
+                            {selectedTag}
+                          </Dropdown.Toggle>
+                          <Dropdown.Menu>
+                            <Dropdown.Item eventKey="Students Organized Events">
+                              Students Organized Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="Professors Organized Events">
+                              Professors Organized Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="Speakers Organized Events">
+                              Speakers Organized Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="Northeastern's Management Events">
+                              Northeastern's Management Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="Khoury College of Computer Science Organized Events">
+                              Khoury College of Computer Science Organized
+                              Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="College of Engineering Organized Events">
+                              College of Engineering Organized Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="College of Professional Studies Organized Events">
+                              College of Professional Studies Organized Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="College of Science Organized Events">
+                              College of Science Organized Events
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="D'Amore College of Management Events">
+                              D'Amore College of Management Events
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </div>
+                      <div className="row">
+                        <label className="lead mt-3 mb-1">
+                          Venue (Enter through Google Maps down below)
+                        </label>
+                        <LocationInput
+                          className="signup-input"
+                          onLocationChange={handleLocationChange}
+                        />
+                      </div>
+                      <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                          <label className="lead mt-3 mb-1">
+                            Start Date and Time
+                          </label>
+                          <div>
+                            <DatePicker
+                              defaultValue={new Date()}
+                              value={startDate}
+                              onChange={handleStartDateChange}
+                              className="w-3/5"
+                            />
+                            <br />
+                            <TimeInput
+                              defaultValue={new Date()}
+                              value={startTime}
+                              onChange={handleStartTimeChange}
+                              className="w-2/5 mt-0"
+                              use12HourClock
+                            />
+                          </div>
+                        </div>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                          <label className="lead mt-3 mb-1">
+                            End Date and Time
+                          </label>
+                          <div>
+                            <DatePicker
+                              defaultValue={new Date()}
+                              value={endDate}
+                              onChange={handleEndDateChange}
+                              className="w-3/5"
+                            />
+                            <br />
+                            <TimeInput
+                              defaultValue={new Date()}
+                              value={endTime}
+                              onChange={handleEndTimeChange}
+                              className="w-2/5 mt-0"
+                              use12HourClock
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <label className="lead mt-3 mb-1">
+                          Event Image URL
+                        </label>
+                        <div className="container">
+                          <TextInput
+                            type="text"
+                            value={formData.imageUrl}
+                            className=""
+                            onChange={(e) =>
+                              handleFieldChange("imageUrl", e.target.value)
+                            }
+                            onBlur={() => handleFieldBlur("imageUrl")}
+                            isInvalid={!!errorValidation.imageUrl}
+                            placeholder={"https://example.com/images/123.jpeg"}
+                          />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <Button
+                          variant="success"
+                          text={"Create Event"}
+                          onClick={handleCreateEvent}
+                          className="mt-3 mb-1"></Button>
+                      </div>
                     </div>
-                  </CustomLabel>
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col>
-                <Form.Group controlId="eventImageURL">
-                  <CustomLabel>
-                    <label className="lead mt-3 mb-1">Event Image Url</label>
-                    <TextInput
-                      type="text"
-                      value={formData.imageUrl}
-                      className=""
-                      onChange={(e) =>
-                        handleFieldChange("imageUrl", e.target.value)
-                      }
-                      onBlur={() => handleFieldBlur("imageUrl")}
-                      isInvalid={!!errorValidation.imageUrl}
-                      placeholder={"https://example.com/images/123.jpeg"}
-                    />
-                  </CustomLabel>
-                </Form.Group>
-              </Col>
-            </Row>
-            <Button
-              variant="danger"
-              text={"Create Event"}
-              onClick={handleCreateEvent}
-              className="mt-3 mb-1"
-            ></Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-2 col-lg-3 col-xl-3"></div>
           </div>
-        </Container>
-      </Form>
+        </div>
+      </div>
       {showAlert && (
         <MyToast
           bg={"danger"}
           show={showAlert}
-          onClose={() => {setShowAlert(false);setError("")}}
+          onClose={() => {
+            setShowAlert(false);
+            setError("");
+          }}
           message={error}
         />
       )}
