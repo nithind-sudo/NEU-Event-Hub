7f6339a881068c6296aaa6784ac7b96aeea8669f
sstej88 <saitejsunkara9@gmail.com>
2023-04-20 21:43:08 -0400

    Attended Events and Upcoming Events updated

---
+++ client/src/pages/UpcomingEvents/UpcomingEvents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Styles/UpcomingEvents.css";
+import Navbar from "../../components/Navbar/Navbar";
+
+const UpcomingEvents = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default UpcomingEvents;
