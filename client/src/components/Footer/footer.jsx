d1beb2d1ec79f03578bb0f7a13eab0c6b2c239d5
kaustubh9702 <kaustubhlawale4@gmail.com>
2023-04-21 18:32:56 -0400

    Updated the footer for the website and added some more fields!

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="bg-dark text-white py-5" id="contact">
       <div className="container">
         <div className="row">
-          <div className="col-lg-4 col-md-4 col-sm-12">
+          <div className="col-lg-4 col-md-4 col-sm-12 mb-4">
             <p className="text-lg-left text-md-left text-center">
               &copy; 2023 Northeastern Events
             </p>
@@ -17,7 +17,7 @@ const Footer = () => {
               <li>FAQs</li>
             </ul>
           </div>
-          <div className="col-lg-4 col-md-4 col-sm-12">
+          <div className="col-lg-4 col-md-4 col-sm-12 mb-4">
             <div className="about text-center">
               <h4>Contact Us</h4>
               <p>
@@ -32,11 +32,11 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="col-lg-4 col-md-4 col-sm-12">
+          <div className="col-lg-4 col-md-4 col-sm-12 mb-4">
             <div className="subscribe text-lg-right text-md-right text-center">
               <h4>Subscribe</h4>
               <form className="px-5">
-                <div className="form-group">
+                <div className="form-group mb-3">
                   <input
                     type="email"
                     className="form-control"
@@ -45,7 +45,32 @@ const Footer = () => {
                     required
                   />
                 </div>
-                <br/>
+                <div className="form-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="First Name"
+                    name="firstName"
+                    required
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Last Name"
+                    name="lastName"
+                    required
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Company"
+                    name="company"
+                  />
+                </div>
                 <div className="form-group">
                   <button type="submit" className="btn btn-primary">
                     Subscribe
